637a4616927a3e60196c0f6f181b18b93a810fb3
cadence <cadencecenters2@gmail.com>
2026-07-23 10:08:40 -0500

    refactor config.py to define hyperparameters and data paths

--- src/config.py
+++ src/config.py
@@ -1,1 +1,20 @@
 # File for hyperparameters
+from pathlib import Path
+
+# Hyperparameters
+Learning_rate = 1e-4
+batch_size = 32
+num_epochs = 50
+train_test_split = 0.7
+
+latent_dimension_size = 4
+beta_value = 2 #weight of KL divergence
+
+# early stopping hyperparameters
+
+project_root = Path(__file__).resolve().parent.parent
+
+# Data paths
+raw_data_dir = project_root / "data" / "raw"
+processed_data_path = project_root / "data" / "processed" / "processed_data.npy"
+model_save_dir = project_root / "models"
