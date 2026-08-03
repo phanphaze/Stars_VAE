7e266ee70007ad9008fb812a998e78782c55b90b
fatima <fatimasyed1007@gmail.com>
2026-08-03 10:25:01 -0500

    Updated config

--- src/config.py
+++ src/config.py
@@ -10,23 +10,30 @@ num_profile_points = 60
 
 # train attributes
 Learning_rate = 1e-4
-batch_size = 32
-num_epochs = 50
+batch_size = 128
+num_epochs = 1000
 train_test_split = 0.8
-beta_value = 2 #weight of KL divergence
+
+# <1 for precise data reconstruction, 
+# =1 for standard proibalistic training, 
+# >1 for clean and separate feature learning)
+beta_value = .3 #weight of KL divergence 
+lambda_value = 1 #weight of MSE divergence
+
 
 # early stopping attributes
-early_stopping_patience = 3 #number of epochs to wait for improvement before stopping
-early_stopping_min_delta = 1e-4 #minimum change in loss to qualify as an improvement
+early_stopping_patience = 30 #number of epochs to wait for improvement before stopping
+early_stopping_min_delta = 1 #minimum change in loss to qualify as an improvement
 
 # model attributes
-input_dimension_size = 2
-output_dimension_size = 2
-latent_dimension_size = 4
+input_dimension_size = num_profile_points * len(profile_features)
+output_dimension_size = num_profile_points * len(profile_features)
+hidden_dimension_1_size = 512
+latent_dimension_size = 8
 
 
 # Data paths
 project_root = Path(__file__).resolve().parent.parent
-raw_data_dir = project_root / "data" / "raw"
+raw_data_path = project_root / "data" / "raw" / "sample_data.npy"
 processed_data_path = project_root / "data" / "processed" / "processed_data.npy"
 model_save_dir = project_root / "models"
